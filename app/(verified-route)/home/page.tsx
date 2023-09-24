c5f35748ccb68efa9ad794c1c68ab20d73b4da5b
krishan singh karki <krishansinghkarki@krishans-MacBook-Air.local>
2023-09-24 21:46:28 +0530

    seprate compnents for the My Rooms

--- app/(verified-route)/home/page.tsx
+++ app/(verified-route)/home/page.tsx
@@ -11,10 +11,8 @@ import { useRouter } from 'next/navigation';
 import { DialogTitle, FormControl, FormLabel, Input, Modal, ModalDialog } from '@mui/joy';
 import { LoaderContext } from '@/context/loader.context';
 import { AuthContext } from '@/context/auth.context';
-import { db } from '@/lib/firebase';
-import Link from 'next/link';
-import { Groups3 } from '@mui/icons-material';
 import { ErrorContext } from '@/context/error.context';
+import MyRooms from '@/components/myRooms.components';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
@@ -28,8 +26,6 @@ export default function Home() {
   const errors = React.useContext(ErrorContext);
 
   const router = useRouter();
-
-  const [myRooms, setMyRooms] = React.useState<Array<any>>([])
 
   const joinRoom = (ev: any) => {
     LoaderService.setShowLoader(true)
@@ -73,15 +69,7 @@ export default function Home() {
     })
   }
 
-  React.useEffect(() => {
-    const unsub = db.collection('userRoomsInfo').doc(auth.username).collection('rooms').onSnapshot((val) => {
-      const data = val.docs.map((val) => val.data())
-      setMyRooms(data)
-      return ()=>{
-        unsub()
-      }
-    })
-  },[])
+  
 
   return (<>
 
@@ -171,29 +159,7 @@ export default function Home() {
       >
         My Rooms
       </Typography>
-      <Box sx={{ bgcolor: 'background.paper', p: 1, display: 'flex', flexWrap: 'wrap' }} component="footer">
-
-        {myRooms.map((room) => <div key={room.id} className="w-2/6 m-5 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <div className="flex flex-col items-center pb-10">
-            <Groups3></Groups3>
-            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-              {room.name}
-            </h5>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              {room.id}
-            </span>
-            <div className="flex mt-4 space-x-3 md:mt-6">
-              <Link
-                href={'/chat?id=' + room.id + '&name=' + room.name}
-                className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Open
-              </Link>
-            </div>
-          </div>
-        </div>)}
-
-      </Box>
+      <MyRooms/>
       {/* End footer */}
     </ThemeProvider>
   </>
---
+++ components/myRooms.components.tsx
@@ -0,0 +1,47 @@
+import { useAuth } from "@/hooks/useAuth"
+import { db } from "@/lib/firebase"
+import { Groups3 } from "@mui/icons-material"
+import { Box } from "@mui/joy"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
+export default function MyRooms() {
+
+    const {auth} = useAuth();
+
+    const [myRooms, setMyRooms] = useState<Array<any>>([])
+    useEffect(() => {
+        const unsub = db.collection('userRoomsInfo').doc(auth.username).collection('rooms').onSnapshot((val) => {
+            const data = val.docs.map((val) => val.data())
+            setMyRooms(data)
+            return () => {
+                unsub()
+            }
+        })
+    }, [])
+    return (
+        <Box sx={{ bgcolor: 'background.paper', p: 1, display: 'flex', flexWrap: 'wrap' }} component="footer">
+
+            {myRooms.map((room) => <div key={room.id} className="w-2/6 m-5 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div className="flex flex-col items-center pb-10">
+                    <Groups3></Groups3>
+                    <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                        {room.name}
+                    </h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                        {room.id}
+                    </span>
+                    <div className="flex mt-4 space-x-3 md:mt-6">
+                        <Link
+                            href={'/chat?id=' + room.id + '&name=' + room.name}
+                            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Open
+                        </Link>
+                    </div>
+                </div>
+            </div>)}
+
+        </Box>
+    )
+}
