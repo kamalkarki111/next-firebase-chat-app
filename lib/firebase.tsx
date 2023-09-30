9fe062565f89311ee6994671c14999200181fc41
krishan singh karki <krishansinghkarki@krishans-MacBook-Air.local>
2023-09-30 15:09:14 +0530

    changes with auth

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -3,14 +3,7 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCV_I00jo6eoGeq81UZ1WoHSgXovyaaJtw",
-    authDomain: "myapp-d9482.firebaseapp.com",
-    databaseURL: "https://myapp-d9482-default-rtdb.firebaseio.com",
-    projectId: "myapp-d9482",
-    storageBucket: "myapp-d9482.appspot.com",
-    messagingSenderId: "286170574856",
-    appId: "1:286170574856:web:15b57a12d0e99c242c61bd",
-    measurementId: "G-7F63HQ918E"
+    //your firebase config goes here
   };
 
 // Initialize Firebase
